1e6ef3e3f20fc31b5ee77b6987a56c720281182b
Compey <hi@devcomp.xyz>
2022-11-09 12:47:45 +0530

    feat(ts + lua): GetFastFlag & GetFastInt docs

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Document, {Html, Head, Main, NextScript} from 'next/document';
 import {SkipNavLink} from '@reach/skip-nav';
 
-export default class HopDocs extends Document {
+export default class DatalinkDocs extends Document {
 	render() {
 		return (
 			<Html lang="en">
